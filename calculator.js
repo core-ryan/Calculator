d7b65ea081f01f3bc24106c3fb8550837603e296
core-ryan <ryanjr0620@yahoo.com>
2023-11-15 19:29:15 -0700

    Added text display box to act as calculator screen Added additional buttons for future functionality Added styling to buttons, background color, and spacing Added JS event listener to react when a button is pressed (currently writes pressed button to text output box)

--- calculator.js
+++ calculator.js
@@ -36,3 +36,20 @@ function operate(operator,num1,num2) {
         return "ERROR";
     }
 }
+
+// Retrieves the text element that acts as a display for the calculator
+const calcScreenText = document.getElementById("text");
+// Retrieves the grid-container that holds all button buttons
+const buttonContainer = document.getElementById("wrapper");
+// Adding event listener to react to when the button interface is clicked
+buttonContainer.addEventListener("click", (event) => {
+    // Stores bool in isButton variable to indicate if it was a button that was pressed
+    // Will be false if space around buttons is pressed
+    const isButton = event.target.nodeName === 'BUTTON';
+    if(!isButton){
+        // Exit function if not button
+        return;
+    }
+    // alert(event.target.innerHTML);
+    calcScreenText.innerHTML = event.target.innerHTML;
+});
